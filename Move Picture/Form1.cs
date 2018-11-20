364b6c54eb044d9854e7ec424400ad721d79f759
Caitlin Cronin <rc0027793@student.riversidecollege.ac.uk>
2018-11-20 10:34:19 +0000

    Task2 // Groot up

--- Move Picture/Form1.cs
+++ Move Picture/Form1.cs
@@ -19,7 +19,8 @@ namespace Move_Picture
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X -5, pictureBox1.Location.Y);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
+
         }
     }
 }
