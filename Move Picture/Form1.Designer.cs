c2b7b7c1295a34ceb631da3f5df0762636abfa24
Caitlin Cronin <rc0027793@student.riversidecollege.ac.uk>
2018-11-26 14:44:02 +0000

    No more off page

--- Move Picture/Form1.Designer.cs
+++ Move Picture/Form1.Designer.cs
@@ -114,6 +114,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
